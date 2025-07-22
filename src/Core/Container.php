5b84a1db832be8023a8bf1bb60ff5e31f670c5c1
goha <gohaogorodnikov@gmail.com>
2025-07-22 12:38:02 +0300

    Done DI-container v1.0

--- src/Core/Container.php
+++ src/Core/Container.php
@@ -2,3 +2,100 @@
 
 declare(strict_types=1);
 
+namespace App\Core;
+
+use App\Core\Binding;
+use Closure;
+use InvalidArgumentException;
+
+/**
+ * Class assembler
+ * 
+ * Can bind and give Classes 
+ */
+class Container
+{
+    /** @var array<string, Binding> */
+    private array $bindings = [];
+
+    /** @var Class[] */
+    private array $sharedInstances = [];
+
+
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * Binds a ClassName with a Closure for later creation in get-method.
+     *
+     * Binds Class which would be creating anew instance every time.
+     * If you need bind statement class use sigleton method.
+     * 
+     * @param string $className name of binding Class
+     * @param Closure $closure instruction for creating Class
+     * @return void
+     */
+    public function bind(string $className, Closure $closure): void
+    {
+        $binding = new Binding($closure, false);
+
+        $this->bindings[$className] = $binding;
+    }
+
+
+    /**
+     * Binds a ClassName with a Closure for later creation in get-method.
+     *
+     * Binds Class which would be creating for once and would be storeged.
+     * If you need bind always anewed class use bind method.
+     * 
+     * @param string $className name of binding Class
+     * @param Closure $closure instruction for creating Class
+     * @return void
+     */
+    public function singleton(string $className, Closure $closure): void
+    {
+        $binding = new Binding($closure, true);
+
+        $this->bindings[$className] = $binding;
+    }
+
+
+    /**
+     * Gives instance of requested Class by ClassName.
+     *
+     * @param string $className requested Class by ClassName
+     * 
+     * @return void
+     * 
+     * @throws InvalidArgumentException ClassName was not binded
+     */
+    public function get(string $className)
+    {
+        $binding = $this->bindings[$className] ?? null;
+
+        if (empty($binding)) {
+            throw new InvalidArgumentException('Not binded Class');
+        }
+        
+        if ($binding->shared === false) {
+            $closure = $binding->closure;
+            return $closure();
+        }
+
+        $isInSharedIstances = isset($this->sharedInstances[$className]);
+
+        if ($isInSharedIstances) {
+            return $this->sharedInstances[$className];
+        }
+
+        $closure = $binding->closure;
+        $instance = $closure();
+
+        $this->sharedInstances[$className] = $instance;
+        return $instance;
+    }
+}
+
