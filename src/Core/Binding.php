f758fb66bceb3bd94de207366353c9f4a2ee579e
goha <gohaogorodnikov@gmail.com>
2025-07-22 10:18:28 +0300

    Implemented Binding, fixed class name

---
+++ src/Core/Binding.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use Closure;
+
+class Binding
+{
+    public function __construct(
+        public readonly Closure $closure,
+        public readonly bool $shared
+    ){}
+}
